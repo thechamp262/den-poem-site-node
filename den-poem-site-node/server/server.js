96a0ba672f74d08c5598020877629e280e8a9b23
thechamp262 <thefuturechamp@gmail.com>
2017-12-07 04:00:44 -0500

    setup initial html and css files using hbs

--- den-poem-site-node/server/server.js
+++ den-poem-site-node/server/server.js
@@ -1,10 +1,23 @@
 require("./config/config");
-
+const path = require('path');
+const hbs = require('hbs');
 const express = require('express');
 const {mongoose} = require('./db/mongoose');
 
 const app = express();
+const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT;
+
+hbs.registerPartials(publicPath + '/views/partials');
+
+app.use(express.static('public'));
+app.set('views', publicPath + '/views');
+app.set('view engine','hbs');
+
+
+app.get('/',(req,res)=>{
+  res.render('admin.hbs');
+})
 
 app.listen(port,function(){
   console.log(`Started on at port ${port}`);
